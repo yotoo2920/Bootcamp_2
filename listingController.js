9c7ae009b2df7dc7ca4615d0e759fa52e330ccfd
yotoo2920 <d.orta.santana@ufl.edu>
2017-09-06 23:18:47 -0400

    added some css

--- listingController.js
+++ listingController.js
@@ -2,13 +2,41 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.newListing = "";
+    $scope.itemToBeFound = "";
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      if ($scope.newListing !== ""){
+        console.log("llegue aqui");
+        $scope.listings.push($scope.newListing);
+        $scope.newListing = "";
+      }
+    };
+
+    $scope.deleteListing = function(index) {
+      console.log(index);
+      $scope.listings.splice(index, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+      console.log(detailedInfo);
+      console.log("show");
+    };
+
+    // $scope.searchItem = function(itemToBeFound) {
+    //   if (itemToBeFound !== "")
+    //   {
+    //     document.getElementById('info').style.display = 'block';
+    //   }
+    //   // else
+    //   // {
+    //   //   delete all listing
+    //   // }
+    // };
   }
 ]);
